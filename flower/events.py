b0c0cedba35da629ef594423fdf420675f9a7c40
Alexei Efimov <alexei.efimov@wirexapp.com>
2021-04-08 13:13:42 +0300

    feat: add alert name to metrics

--- flower/events.py
+++ flower/events.py
@@ -41,7 +41,6 @@ class EventsState(State):
         self.metrics = PrometheusMetrics()
 
     def event(self, event):
-        print(event)
         worker_name = event['hostname']
         event_type = event['type']
 
@@ -54,9 +53,11 @@ class EventsState(State):
                 alert_name = ast.literal_eval(event['args'])
                 alert_name = alert_name[0].get('search_name', '')
             except:
-                alert_name = 'no name'
+                pass
             if not task_name and task_id in self.tasks:
                 task_name = self.tasks[task_id].name or ''
+            if not alert_name and task_id in self.tasks:
+                alert_name = self.tasks[task_id].args or ''
             self.metrics.events.labels(
                 worker_name, event_type, task_name, alert_name).inc()
 
